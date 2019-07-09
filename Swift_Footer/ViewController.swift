995007c9767d36aa4d4bab55c851c27457c7daf2
kavindu <chathurangakavindu67@gmail.com>
2019-07-09 14:19:50 +0530

    Initial Commit

---
+++ Swift_Footer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_Footer
+//
+//  Created by kavii on 7/9/19.
+//  Copyright © 2019 kavii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
